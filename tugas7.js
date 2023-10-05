97af10423844dc9badc18d39faa6957b410d9cb4
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 14:46:13 +0700

    Tugas 7 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,35 @@
+var TinggiBadan = [
+  "Dany",
+  "tinggibadan",
+  173, // 01
+  "Hendry",
+  "tinggibadan",
+  160, //02
+  "Doni",
+  "tinggibadan",
+  165, //03
+  "Listi",
+  "tinggibadan",
+  164, //04
+  "Merlinda",
+  "tinggibadan",
+  155, // 05
+  "Yuki",
+  "tinggibadan",
+  167, //06
+  "Cindy",
+  "tinggibadan",
+  150, //07
+  "Kentaro",
+  "tinggibadan",
+  176, //08
+  "Rentaro",
+  "tinggibadan",
+  169, //09
+  "JOKO",
+  "tinggibadan",
+  190, //10
+];
+for (let x of TinggiBadan) {
+  console.log(x);
+}
